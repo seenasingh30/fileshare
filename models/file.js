53ba8a61a8f3d4e1d42e393686bea53058be807c
sakshisingh14 <115209708+sakshisingh14@users.noreply.github.com>
2022-10-11 17:13:39 +0530

    Update file.js

--- models/file.js
+++ models/file.js
@@ -5,27 +5,27 @@ const Schema = mongoose.Schema;
 const fileSchema = new Schema({
  filename:{
     type : String,
-    required : true
+    
  },
  pathname:{
     type : String,
-    required : true
+    
  },
 size:{
     type : Number,
-    required : true
+   
  },
  uuid:{
     type :String,
-    required : true
+    
  },
  sender:{
     type :String,
-    required : false
+   
  },
  receiver:{
     type :String,
-    required : false
+    
  }
 
 } ,{timestamps : true});
